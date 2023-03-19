681d052bd7de848d4a696bf520d52a7a6d66c556
RomanPrograma <126255710+RomanPrograma@users.noreply.github.com>
2023-03-19 07:45:22 +0200

    ex_3 done

--- variables.py
+++ variables.py
@@ -1,1 +1,9 @@
 
+num_1 = int(input("write number"))
+num_2 = int(input("write number"))
+result_1 = num_1 + num_2
+result_2 = num_1 - num_2
+result_3 = num_1 * num_2
+print("sum", result_1)
+print("difference", result_2)
+print("product", result_3)
